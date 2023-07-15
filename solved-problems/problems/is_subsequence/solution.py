9ea3501f611af04335deeab2600b94227e8f0ad6
V Gokul Ajith <gokulvasu225@gmail.com>
2023-07-15 19:19:29 +0000

    Sync LeetCode submission - Is Subsequence (python3)

--- solved-problems/problems/is_subsequence/solution.py
+++ solved-problems/problems/is_subsequence/solution.py
@@ -1,12 +1,27 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+
         i = 0
         j = 0
-        m = len(s)
-        n = len(t)
-        while j < n and i < m:
-            if s[i] == t[j]:
+
+        while j < len(t) :
+
+            if(i == len(s)):
+                return True 
+
+            if(s[i] == t[j]):
                 i += 1
-            j += 1
-        return i == m
-        
+                j += 1
+
+            else :
+                j += 1
+
+        if(i == len(s)):
+            return True 
+        else :
+            return False                
+
+
+
+
+                 
