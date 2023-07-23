89074858f7c1448acae123c52a2925fe3fbd78f3
V Gokul Ajith <gokulvasu225@gmail.com>
2023-07-23 06:14:51 +0000

    Sync LeetCode submission - Move Zeroes (python3)

--- solved-problems/problems/move_zeroes/solution.py
+++ solved-problems/problems/move_zeroes/solution.py
@@ -3,11 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l = 0
-        for r in range(len(nums)):
-            if nums[r] :
-                nums[l],nums[r] = nums[r],nums[l]
-                l += 1
+        i = 0
+        
+        for j in range(1,len(nums)):
+
+            if(nums[i] == 0 and nums[j] != 0):
+                nums[i],nums[j] = nums[j],nums[i]
+                i += 1
+
+            elif(nums[i] != 0):
+                i += 1
+
+                
+
+
+
+
+
             
         
 
