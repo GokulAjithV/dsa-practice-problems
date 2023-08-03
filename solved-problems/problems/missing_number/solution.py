ca5242e376a134e2e7cbefb60a0f19f09b8c0fe2
V Gokul Ajith <gokulvasu225@gmail.com>
2023-08-03 06:21:50 +0000

    Sync LeetCode submission - Missing Number (python3)

--- solved-problems/problems/missing_number/solution.py
+++ solved-problems/problems/missing_number/solution.py
@@ -1,6 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums) + 1):
-            if i not in nums :
-                return i
+        
+        n = len(nums)
+        ans = 0
+        for i in range(len(nums)):
+
+            ans = ans^nums[i]^i
+
+        return ans^n
+
+        
 
