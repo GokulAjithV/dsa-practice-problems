f6221f31f23f5cb32f76331e57144be5f164b460
V Gokul Ajith <gokulvasu225@gmail.com>
2023-08-04 17:28:01 +0000

    Sync LeetCode submission - Subsets (python3)

--- solved-problems/problems/subsets/solution.py
+++ solved-problems/problems/subsets/solution.py
@@ -1,15 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
 
+        n = len(nums)
         ans = []
-        def solve(i, curr):
-            if i >= len(nums):
-                ans.append(curr.copy())
-                return 
-            curr.append(nums[i])
-            solve(i+1, curr)
+        def helper(i,arr):
 
-            curr.pop()
-            solve (i+1, curr)
-        solve(0,[])
-        return ans
+            if(i == n):
+                ans.append(arr[:])
+                return 
+
+            arr.append(nums[i])
+            helper(i+1,arr)
+            arr.remove(nums[i])
+            helper(i+1,arr)
+        helper(0,[])
+        return ans        
+                
