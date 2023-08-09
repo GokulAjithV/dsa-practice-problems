ba2a15d6f3bf39b14e8936189e1585674dc3095d
V Gokul Ajith <gokulvasu225@gmail.com>
2023-08-09 07:30:03 +0000

    Sync LeetCode submission - Minimum Path Sum (python3)

--- solved-problems/problems/minimum_path_sum/solution.py
+++ solved-problems/problems/minimum_path_sum/solution.py
@@ -1,25 +1,31 @@
 class Solution:
-    def minPathSum(self, grid: List[List[int]]) -> int:
-
-        def dfs(i,j):
-
-            if(i == 0 and j == 0):
-                return grid[0][0]
-
-            if(i < 0 or j < 0):
-                return 300
-
-            if(dp[i][j] != -1):
-                return dp[i][j]
-
-            up = grid[i][j] + dfs(i-1,j)
-            left = grid[i][j] + dfs(i,j-1)
-
-            dp[i][j] = min(up,left)
-
-            return dp[i][j]
+    def minPathSum(self, grid: List[List[int]]) -> int:  
 
         m = len(grid)
-        n = len(grid[0])  
-        dp = [[-1]*n for _ in range(m)]
-        return dfs(m-1,n-1)    
+        n = len(grid[0])
+
+        def helper(i,j):
+            
+            if(i == 0 and j == 0):
+                return grid[i][j]
+
+            if(i < 0 or j < 0):
+                return 1e9
+
+            if(dp[i][j] != -1):
+                return dp[i][j]   
+
+            up = grid[i][j] + helper(i-1,j)
+            left = grid[i][j] + helper(i,j-1)
+            
+            dp[i][j] = min(up,left)
+
+            return min(up,left)
+
+        dp = [[-1]*n for _ in range(m)]    
+
+        return helper(m-1,n-1)
+
+
+
+        
