d3fa000997dbc0a5c779c5e249630657b67ffcc3
V Gokul Ajith <gokulvasu225@gmail.com>
2023-08-09 01:15:55 +0000

    Sync LeetCode submission - House Robber II (python3)

--- solved-problems/problems/house_robber_ii/solution.py
+++ solved-problems/problems/house_robber_ii/solution.py
@@ -2,35 +2,34 @@ class Solution:
 
     def rob(self, nums: List[int]) -> int:
 
-        
-        
-        def dfs(n,arr,dp):
+        n = len(nums)
 
-            dp[0] = arr[0]
-            prev = 0
-
-            for i in range(1,len(arr)):
-
-                take = arr[i]
-
-                if(i > 1):
-                    take += dp[i-2]
-
-                notTake = dp[i-1]
-
-                dp[i] = max(take,notTake)
-
-            return dp[n]
-        
-
-
-        if(len(nums) == 1):
+        if(n == 1):
             return nums[0]
 
-        
-        wf = nums[1:]
-        wl = nums[:len(nums)-1]
-        return max(dfs(len(wf)-1,wf,{}),dfs(len(wl)-1,wl,{}))
+        def helper(i,j,dp):
+
+            if(i == j):
+                return nums[i]
+
+            if(i < j):
+                return 0
+
+            if(dp[i] != -1):
+                return dp[i]    
+
+            pick = nums[i] + helper(i-2,j,dp)
+
+            notPick = 0 + helper(i-1,j,dp)
+            
+            dp[i] = max(pick,notPick) 
+
+            return max(pick,notPick)
+
+        dp1 = [-1]*n
+        dp2 = [-1]*n    
+
+        return max(helper(n-2,0,dp1),helper(n-1,1,dp2))            
 
 
 
