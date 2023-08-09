746c1cb80bd65493676c44ad8de1adb62c2fbb6c
V Gokul Ajith <gokulvasu225@gmail.com>
2023-08-09 05:50:57 +0000

    Sync LeetCode submission - Unique Paths (python3)

--- solved-problems/problems/unique_paths/solution.py
+++ solved-problems/problems/unique_paths/solution.py
@@ -1,25 +1,27 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        def dfs(i,j):
+        def helper(i,j):
 
             if(i == 0 and j == 0):
                 return 1
 
-            if(i < 0 or j < 0):
+            if(i < 0 or j < 0 ):
                 return 0
 
             if(dp[i][j] != -1):
                 return dp[i][j]    
-
-            up = dfs(i-1,j)
-            left = dfs(i,j-1)
             
-            dp[i][j] = up + left 
+            up = helper(i-1,j)
+            left = helper(i,j-1)
 
-            return  dp[i][j]
+            dp[i][j] = up + left
 
-        dp = [[-1]*n for _ in range(m)]    
+            return up + left
 
-        return dfs(m-1,n-1)        
+        dp = [[-1]*n for _ in range(m)]     
+
+        return helper(m-1,n-1)            
+
+
 
