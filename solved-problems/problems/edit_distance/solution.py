dd9bd9348d17a6496d94a153c0171e781060726c
V Gokul Ajith <gokulvasu225@gmail.com>
2023-08-12 12:13:04 +0000

    Sync LeetCode submission - Edit Distance (python3)

--- solved-problems/problems/edit_distance/solution.py
+++ solved-problems/problems/edit_distance/solution.py
@@ -1,28 +1,36 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
 
-        def helper(i,j):
-
-            if(i < 0):
-                return j+1
-
-            if(j < 0):
-                return i+1
-
-            if(dp[i][j] != -1):
-                return dp[i][j]
-
-            if(word1[i] == word2[j]):
-                dp[i][j] = helper(i-1,j-1)
-                return dp[i][j]
-
-            else :
-                dp[i][j] = 1 + min(helper(i-1,j),helper(i,j-1),helper(i-1,j-1))
-                return dp[i][j]
-        
         m = len(word1)
         n = len(word2)
 
-        dp = [[-1]*(n) for _ in range(m)]
+        def helper(i,j):
 
-        return helper(m-1,n-1)                               
+            if(i < 0):
+                return j + 1
+
+            if(j < 0):
+                return i + 1
+
+            if(dp[i][j] != -1):
+                return dp[i][j]    
+
+            if(word1[i] == word2[j]):
+                count = helper(i-1,j-1)
+
+            else:
+                add = 1 + helper(i,j-1)
+                delete = 1 + helper(i-1,j)
+                replace = 1 + helper(i-1,j-1)
+
+                count = min(add,delete,replace)
+
+            dp[i][j] = count     
+
+            return dp[i][j]
+        
+        dp = [[-1]*(n+1) for _ in range(m+1)]
+
+        return helper(m-1,n-1)
+
+
