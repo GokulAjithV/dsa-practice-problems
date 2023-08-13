0eb89faa4a044d719d8f197a2c1bf900eea6d96e
V Gokul Ajith <gokulvasu225@gmail.com>
2023-08-13 13:30:28 +0000

    Sync LeetCode submission - Count Square Submatrices with All Ones (python3)

--- solved-problems/problems/count_square_submatrices_with_all_ones/solution.py
+++ solved-problems/problems/count_square_submatrices_with_all_ones/solution.py
@@ -4,7 +4,7 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
 
-        dp = [[0]*(n) for _ in range(m)]
+        dp = [[0]*n for _ in range(m)]
 
         for i in range(m):
             dp[i][0] = matrix[i][0]
@@ -14,12 +14,11 @@ class Solution:
 
         for i in range(1,m):
             for j in range(1,n):
-
                 if(matrix[i][j] == 0):
                     dp[i][j] = 0
-
-                else :
-                    dp[i][j] = 1 + min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1])
+                else:
+                    dp[i][j] = 1 + min(dp[i-1][j],dp[i-1][j-1],dp[i][j-1])
+                                  
 
         Sum = 0
         for i in range(m):
