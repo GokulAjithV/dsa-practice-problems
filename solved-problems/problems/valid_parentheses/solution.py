c8a6b3eb974eb0e0328db6d84b56957d512b95eb
V Gokul Ajith <gokulvasu225@gmail.com>
2026-02-05 03:57:55 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- solved-problems/problems/valid_parentheses/solution.py
+++ solved-problems/problems/valid_parentheses/solution.py
@@ -1,20 +1,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        
-        for i in s:
-            if i in ['(','[','{']:
-                stack.append(i)
-            elif i == ')' and len(stack) != 0 and stack[-1] == '(': 
-                stack.pop()
-            elif i == ']' and len(stack) != 0 and stack[-1] == '[' :
-                stack.pop()
-            elif i == '}' and len(stack) != 0 and stack[-1] == '{' :
-                stack.pop()
+        parentheses_stack = []
+        parentheses_set = {"{", "[", "("}
+        top = None    
+        for p in s:
+            if p in parentheses_set:
+                parentheses_stack.append(p)
+                continue
+            if parentheses_stack: 
+                top = parentheses_stack[-1]
             else:
-                return False 
-        if len(stack) == 0:
-            return True 
-        else:
-            return False 
+                return False
+            if (p == "}") and (top == "{"):
+                parentheses_stack.pop()
+            elif (p == ")") and (top == "("):
+                parentheses_stack.pop()
+            elif (p == "]") and (top == "["):
+                parentheses_stack.pop()
+            else:
+                return False
+        if (len(parentheses_stack) == 0):
+            return True
+        return False
+
+                
         
