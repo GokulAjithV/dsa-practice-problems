226822b0ea4475845d03fca74e0e996c929770c2
GokulAjithV <gokulvasu225@gmail.com>
2026-02-06 10:12:11 +0530

    mountain pattern

--- patterns/patterns.py
+++ patterns/patterns.py
@@ -14,3 +14,18 @@ def right_angle_pattern(row_count):
         for j in range(i + 1):
             print("*", end="")
         print()
+
+
+# 3. Mountain Pattern
+
+def mountain_pattern(row_count):
+    for i in range(row_count):
+        for j in range(row_count - i - 1, 0, -1):
+            print(" ", end="")
+        for k in range(i+1):
+            print("*", end="")
+        for l in range(row_count - i - 1, 0, -1):
+            print(" ", end="")
+        print()
+
+mountain_pattern(5)
