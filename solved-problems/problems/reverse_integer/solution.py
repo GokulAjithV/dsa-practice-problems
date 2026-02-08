544efbbb1d686eb7b1acda3c4478e66873421db2
V Gokul Ajith <gokulvasu225@gmail.com>
2026-02-08 12:35:45 +0000

    Sync LeetCode submission - Reverse Integer (python3)

--- solved-problems/problems/reverse_integer/solution.py
+++ solved-problems/problems/reverse_integer/solution.py
@@ -3,29 +3,22 @@ class Solution:
 
         if (x in [0, 1, -1]):
             return x
-        
-        is_negative = x < 0
 
-        stringified_input = ""
+        reserved_input = 0
+
+        is_negative = x < 0
 
         input_integer = -x if is_negative else x
 
-        while True:
-            last_digit = input_integer % 10
-            if (last_digit == 0):
-                input_integer = input_integer // 10
-            else: 
-                break
-
         while input_integer > 0:
-            last_digit = input_integer % 10
-            stringified_input += str(last_digit)    
+            last_digit = input_integer % 10    
             input_integer = input_integer // 10
+            reserved_input = (reserved_input * 10) + last_digit
 
-        result = -int(stringified_input) if is_negative else int(stringified_input)
+        reserved_input = -reserved_input if is_negative else reserved_input
 
-        if (-2**31 > result or result >= 2**31):
+        if (-2**31 > reserved_input or reserved_input >= 2**31):
             return 0
 
-        return result
+        return reserved_input
 
