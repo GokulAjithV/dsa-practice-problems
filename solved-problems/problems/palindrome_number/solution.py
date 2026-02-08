b86306ea728fe103dc30715b7eb6515872e76866
V Gokul Ajith <gokulvasu225@gmail.com>
2026-02-08 13:00:12 +0000

    Sync LeetCode submission - Palindrome Number (python3)

--- solved-problems/problems/palindrome_number/solution.py
+++ solved-problems/problems/palindrome_number/solution.py
@@ -1,17 +1,17 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0 : 
-            return False 
-        div = 1
-        while x >= 10 * div :
-            div *= 10
-            
-        while x:
-            right = x % 10
-            left = x // div
-            
-            if right != left :
-                return False 
-            x = (x % div) // 10
-            div = div / 100
-        return True     
+
+        if x < 0:
+            return False
+        if x < 10:
+            return True
+        
+        input_int = x
+        reversed_int = 0
+
+        while input_int > 0:
+            last_digit = input_int % 10
+            input_int = input_int // 10
+            reversed_int = (reversed_int * 10) + last_digit
+
+        return reversed_int == x
